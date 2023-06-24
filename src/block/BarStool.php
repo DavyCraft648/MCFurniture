252daa010b206a99b133b12f6c2ba1bd4ccff367
ZhorifCraft451 <75656616+ZhorifCraft451@users.noreply.github.com>
2023-06-24 11:33:24 +0700

    add sit for barstool

--- src/block/BarStool.php
+++ src/block/BarStool.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace DavyCraft648\MCFurniture\block;
 
+use DavyCraft648\MCFurniture\utils\SitUtils;
 use pocketmine\item\Item;
 use pocketmine\math\Vector3;
 use pocketmine\player\Player;
@@ -10,6 +11,9 @@ use pocketmine\player\Player;
 class BarStool extends \pocketmine\block\Transparent{
 
 	public function onInteract(Item $item, int $face, Vector3 $clickVector, ?Player $player = null, array &$returnedItems = []) : bool{
-		return false; //TODO: sit
+		if (!SitUtils::isToggleSit($player)) {
+			SitUtils::sit($player, $this);
+		}
+		return false;
 	}
 }
