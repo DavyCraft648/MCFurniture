4b963e1ab6e940c838337bb18839553c47e9017a
ZhorifCraft451 <75656616+ZhorifCraft451@users.noreply.github.com>
2023-06-24 20:05:48 +0700

    Update Shower.php

--- src/block/Shower.php
+++ src/block/Shower.php
@@ -12,6 +12,30 @@ use pocketmine\data\bedrock\block\convert\BlockStateWriter;
 use pocketmine\data\runtime\RuntimeDataDescriber;
 use pocketmine\nbt\tag\CompoundTag;
 
-class Shower extends \pocketmine\block\Transparent{
+class Shower extends \pocketmine\block\Transparent implements Permutable
+{
 	use HorizontalFacingTrait;
+
+	public function getBlockProperties() : array{
+		return [
+			new BlockProperty(BlockStateNames::FACING_DIRECTION, [2, 3, 4, 5])
+		];
+	}
+	public function getPermutations() : array{
+		return [];
+	}
+	public function getCurrentBlockProperties() : array{
+		return [$this->facing];
+	}
+	protected function describeBlockOnlyState(RuntimeDataDescriber $w) : void{
+		$w->horizontalFacing($this->facing);
+	}
+
+	public function serializeState(BlockStateWriter $blockStateOut) : void{
+		$blockStateOut->writeHorizontalFacing($this->getFacing());
+	}
+
+	public function deserializeState(BlockStateReader $blockStateIn) : void{
+		$this->setFacing($blockStateIn->readHorizontalFacing());
+	}
 }
