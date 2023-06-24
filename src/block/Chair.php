d0ff690edf3d4e1d017d6938b38950fe0613c961
ZhorifCraft451 <75656616+ZhorifCraft451@users.noreply.github.com>
2023-06-24 14:39:23 +0700

    Update src/block/Chair.php
    Co-authored-by: DavyCraft648 <67502532+DavyCraft648@users.noreply.github.com>

--- src/block/Chair.php
+++ src/block/Chair.php
@@ -100,9 +100,9 @@ class Chair extends Transparent implements \customiesdevs\customies\block\permut
 	}
 
 	public function onInteract(Item $item, int $face, Vector3 $clickVector, ?Player $player = null, array &$returnedItems = []) : bool{
-        if (!SitUtils::isSitting($player)) {
-            SitUtils::sit($player, $this);
-        }
+		if(!SitUtils::isSitting($player)){
+			SitUtils::sit($player, $this);
+		}
 		return false;
 	}
 
