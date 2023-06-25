f1f3afd413f4adf128c8a701f7ead16bf484becc
ZhorifCraft451 <75656616+ZhorifCraft451@users.noreply.github.com>
2023-06-25 15:44:29 +0700

    Update ShowerHead.php

--- src/block/ShowerHead.php
+++ src/block/ShowerHead.php
@@ -2,16 +2,14 @@
 
 namespace DavyCraft648\MCFurniture\block;
 
-use customiesdevs\customies\block\permutations\BlockProperty;
-use customiesdevs\customies\block\permutations\Permutation;
+use customiesdevs\customies\block\permutations\Permutable;
+use pocketmine\block\Transparent;
 use pocketmine\block\utils\HorizontalFacingTrait;
-use pocketmine\data\bedrock\block\BlockStateNames;
 use pocketmine\data\bedrock\block\convert\BlockStateReader;
 use pocketmine\data\bedrock\block\convert\BlockStateWriter;
 use pocketmine\data\runtime\RuntimeDataDescriber;
-use pocketmine\nbt\tag\CompoundTag;
 
-class ShowerHead extends \pocketmine\block\Transparent implements \customiesdevs\customies\block\permutations\Permutable{
+class ShowerHead extends Transparent implements Permutable{
 	use HorizontalFacingTrait;
 
 	public function getBlockProperties() : array{
@@ -24,13 +22,10 @@ class ShowerHead extends \pocketmine\block\Transparent implements \customiesdevs
 	public function getCurrentBlockProperties() : array{
 		return [];
 	}
-
-	protected function describeBlockOnlyState(RuntimeDataDescriber $w) : void{
-	}
-
 	public function serializeState(BlockStateWriter $blockStateOut) : void{
 	}
-
 	public function deserializeState(BlockStateReader $blockStateIn) : void{
+	}
+	protected function describeBlockOnlyState(RuntimeDataDescriber $w) : void{
 	}
 }
